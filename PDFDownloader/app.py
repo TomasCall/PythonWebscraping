fc987c07c4b9c12ee940830085e2f0a94dfece45
Vas Bence <vas.bence01@gmail.com>
2022-04-03 08:33:36 +0200

    PDFDownloader

---
+++ PDFDownloader/app.py
@@ -0,0 +1,11 @@
+import os
+
+
+def main():
+    url = "https://docplayer.hu/storage/30/14452822/1648970103/Y-HfOrb6nWBNabkpB90U_w/14452822.pdf"
+    os.system(f"wget {url}")
+
+
+
+if __name__=="__main__":
+    main()
